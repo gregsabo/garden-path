e6c0a041ad8cc0f3abd97e38c649f815bb625231
Greg Sabo <gregsabo@gmail.com>
2023-10-14 15:04:53 -0500

    Saves concepts

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
+"""
+Garden-path
+
+Generate a concept for a full-length novel, then
+generate it in its entirety, by only prompting
+with the last sentence of the previous paragraph.
+"""
 import os
+from datetime import datetime
 from pprint import pprint
 from dotenv import load_dotenv
 import openai
+from lxml import etree
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def main():
+    concept = generate_concept()
+    print(concept)
+    save_concept(concept)
+
+
+def save_concept(concept):
+    """Save the concept to a file."""
+    tree = etree.fromstring(concept)
+    title_tag = tree.xpath('.//title')[0]
+    assert title_tag is not None
+    title_text = title_tag.text.strip()
+    # make title text lowercase, replace whitespace with _
+    title_text = title_text.lower().replace(' ', '_')
+    # prepend timestamp ISO
+    title_text = f'{datetime.now().isoformat()}_{title_text}'
+
+    output_dir = f'output/{title_text}'
+    os.makedirs(output_dir, exist_ok=True)
+    file_path = os.path.join(output_dir, 'concept.xml')
+    with open(file_path, 'w') as file:
+        file.write(concept)
+
+
+def generate_concept():
+    """Generate a concept for a full-length novel."""
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
@@ -36,7 +70,7 @@ DO make the genre dramatic literary fiction appropriate for a Pulitzer."""
         frequency_penalty=0,
         presence_penalty=0,
     )
-    print(response.choices[0].message.content)
+    return response.choices[0].message.content
 
 
 if __name__ == "__main__":
