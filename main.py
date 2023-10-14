12e97b2e247569e36f57d31e26dd7f387c232abf
Greg Sabo <gregsabo@gmail.com>
2023-10-14 14:45:36 -0500

    generates concepts somewhat well.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
+from pprint import pprint
 from dotenv import load_dotenv
 import openai
 
 load_dotenv()
-print("Env", os.getenv("OPENAI_API_KEY"))
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
@@ -13,7 +13,20 @@ def main():
         messages=[
             {
                 "role": "system",
-                "content": "Generate a concept for a full-length novel.\nDO encode your entire response in XML.\nDO make the root tag <novelConcept>.\nDO include <thought>, <title>, <cover>, <firstSentence> tags within the <novelConcept> tag.\nDO make the <thought> tag a short brainstorm of three dramatically different directions.\nDO make the <title> the title of the book.\nDO make the <cover> tag a Midjourney prompt for generating the cover art.\nDO make the <firstSentence> tag the first sentence of the book.\nDO make the genre dramatic literary fiction appropriate for a Pulitzer.",
+                "content": """
+Generate a concept for a full-length novel.
+DO enocde your entire response in XML.
+DO make the root tag <novelConcept>.
+DO include <rejectedIdeas>, <acceptedIdea>, <title>, <cover>, <firstSentence>
+    tags within the <novelConcept> tag.
+DO make the <rejectedIdeas> tag a short brainstorm of three dramatically
+    different directions.
+DO make the <acceptedIdea> tag a fourth idea that's better
+    than the rejected ones.
+DO make the <title> the title of the book.
+DO make the <cover> tag a Midjourney prompt for generating the cover art.
+DO make the <firstSentence> tag the first sentence of the book.
+DO make the genre dramatic literary fiction appropriate for a Pulitzer."""
             },
             {"role": "user", "content": ""},
         ],
@@ -23,3 +36,8 @@ def main():
         frequency_penalty=0,
         presence_penalty=0,
     )
+    print(response.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    main()
